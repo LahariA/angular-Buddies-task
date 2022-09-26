9f6dab3070d8e4eb2d67eb54e8f9420ec28ebd6a
LahariA <46395891+LahariA@users.noreply.github.com>
2022-09-26 17:54:47 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-Buddies-task
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-jgysa1)
---
+++ src/buddies/buddies.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-buddies',
+  templateUrl: './buddies.component.html',
+  styleUrls: ['./buddies.component.css']
+})
+export class BuddiesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
